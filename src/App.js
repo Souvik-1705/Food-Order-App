338e2a37ef0221b072bea6df0192eabd9e421eb1
Souvik-1705 <souvik.techdev1705@gmail.com>
2025-01-31 23:38:17 +0530

    Updated changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React ,{ Fragment } from 'react';
+import React ,{ Fragment,useState } from 'react';
 import './App.css';
 import { Header } from './Components/Layout/Header';
 import  { Meals }  from './Components/meals/Meals';
 import Cart from "./Components/Cart/Cart";   
 
 function App() {
+
+  const [cartIsShown,setCartIsShown]=useState(false);
+
+  const showCartHandler=()=>{
+    setCartIsShown(true);
+  }
+  const hiddenCartHandler=()=>{
+    setCartIsShown(false);
+  }
   return (
     <div className="App">
      <Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hiddenCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>

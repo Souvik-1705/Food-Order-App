2fab73cf29356cbb6c23820717c46497250cb24e
Souvik-1705 <souvik.techdev1705@gmail.com>
2025-01-31 23:18:26 +0530

    Updated changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React ,{ Fragment } from 'react';
 import './App.css';
 import { Header } from './Components/Layout/Header';
 import  { Meals }  from './Components/meals/Meals';
+import Cart from "./Components/Cart/Cart";   
 
 function App() {
   return (
     <div className="App">
      <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
